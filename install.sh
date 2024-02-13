fcdf06d9a96cd164455b60a117a5a0c0148466a5
fab1opinto <fabiopintoapt@gmail.com>
2024-02-13 12:15:44 -0300

    composer

--- install.sh
+++ install.sh
@@ -27,6 +27,19 @@ sudo apt  install golang-go -y
 
 # php
 sudo apt install php8.1-cli -y
+sudo apt install php-mbstring -y
+sudo apt install php-dom -y
+sudo apt install php-curl -y
+
+
+#composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'edb40769019ccf227279e3bdd1f5b2e9950eb000c3233ee85148944e555d97be3ea4f40c3c2fe73b22f875385f6a5155') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar --version
+#tornando o composer global
+sudo mv composer.phar /usr/local/bin/composer 
 
 #lampp
 wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/8.0.30/xampp-linux-x64-8.0.30-0-installer.run/download
@@ -47,6 +60,14 @@ sudo snap install dbeaver-ce
 # sqlite3
 sudo apt install sqlite3 -y
 sudo apt install sqlitebrowser -y
+
+#mariadb
+sudo apt update -y
+sudo apt install mariadb-server -y
+
+#mysql
+sudo apt update -y
+sudo apt install mysql-server -y
 
 #devops, virtualização, monitoramento, containers e orquestração, iac | virtualization, monitoring, containers and orchestration, iac
 #vagrant
